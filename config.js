942cdf22244ca9c811f428da6b0a2930a5744b8e
thientran1199 <thientran1111999@gmail.com>
2022-01-05 14:32:27 +0700

    em

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    introTitle: 'Em  à!',
+    introDesc: `Trái đất vốn lạ thường.
+           Mà sao em cứ đi nhầm đường. 
+        Lạc vào tim anh lẻ loi.
+        Đằng sau chữ yêu đây là chữ thương🧡 `,
+    btnIntro: 'hí hí 🧡',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn thích Anh 🧡 <3',
+    btnNo: 'Không, Bạn à :3',
+    question: 'Trên thế giới có cả  tỉ người mà sao em lại yêu anh thế này 🧡',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Ô! Bé vào rồi này😘😘',
+    messDesc: ':)))',
+    btnAccept: 'Okiiiii lun😘🧡',
+    messLink: 'https://www.facebook.com/thientran111'
+        //link mess của các bạn. VD: https://www.facebook.com/thientran111
+}
